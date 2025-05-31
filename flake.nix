aaa8c1a77ede618fb2405063b8f585c34dc8b232
permalik <permalik@protonmail.com>
2025-05-31 15:52:46 -0500

    feat: init

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "lark";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nix-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nix-unstable,
+    flake-utils,
+    zig-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        zigPackage = zig-overlay.packages.${system}."0.14.0";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell {
+          name = "lark";
+          packages = with pkgs; [
+            alejandra
+          ];
+          nativeBuildInputs = [
+            zigPackage
+          ];
+
+          shellHook = with pkgs; ''
+            # Source .bashrc
+            . .bashrc
+          '';
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "lark";
+          src = ./.;
+
+          XDG_CACHE_HOME = "${placeholder "out"}";
+
+          # buildInputs = [];
+          buildPhase = ''
+            ${zigPackage}/bin/zig build
+          '';
+
+          installPhase = ''
+            ${zigPackage}/bin/zig build install --prefix $out
+            rm -rf $out/zig
+          '';
+        };
+      }
+    );
+}
